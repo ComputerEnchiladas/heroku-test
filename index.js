7bc1e038048e7bbeb94c7cc339fc696204a7dfc5
JesusGuerrero <rocha.jesus@hotmail.com>
2018-02-08 23:27:55 -0800

    My First Commit
    Adding a description

---
+++ index.js
@@ -0,0 +1,12 @@
+require('mahrio').runServer( process.env, __dirname) 
+  .then( function(server){ 
+    server.route({ 
+      path: '/', 
+      method: 'GET', 
+      handler: function(req, rep){ 
+        rep('Hello World! It is ' + (new Date())); 
+      } 
+    }); 
+  });
+
+  // Server runs http://127.0.0.1:6085
